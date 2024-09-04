b017e467e634131dfa64627de4ab1c7bd62932bc
jakiichu4 <zellda444@gmail.com>
2024-09-04 17:58:53 +0700

    asd

--- src/pages/main_page/index.tsx
+++ src/pages/main_page/index.tsx
@@ -1,7 +1,9 @@
 const MainPage = () => {
     return (
-        <div>
-            MainPage
+        <div className='flex items-center justify-center min-h-screen'>
+            <button className='p-2 bg-gray-900 text-white rounded' onClick={()=>{
+                window.location.href = 'http://localhost:5173/'
+            }}>localhost:5173</button>
         </div>
     );
 };
